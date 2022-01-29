5810cd7bc2aa01f8c5d15350aea0b22c9b5449fb
Amanda <amanda.nilsson93@gmail.com>
2022-01-29 12:57:09 +0100

    Created NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import NavBar from "./components/Navbar";
 
 function App() {
   return (
     <div>
+      <NavBar />
       <h1>Windbooking</h1>
     </div>
   );
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Windparks</li>
+        <li>Login</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
