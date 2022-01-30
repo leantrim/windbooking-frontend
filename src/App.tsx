a3add2171fc22d9419daa2d04299f377c4412071
Amanda <amanda.nilsson93@gmail.com>
2022-01-30 17:56:29 +0100

    created Windparks and Login component plus routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import Windparks from "./components/Windparks";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <h1>Windbooking</h1>
       <Switch>
         <Route path="/home" component={Home} />
+        <Route path="/windparks" component={Windparks} />
+        <Route path="/login" component={Login} />
       </Switch>
     </div>
   );
