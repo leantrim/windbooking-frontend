f685814746209e95f6e110549b9a499b3bda97c1
albin <albinp2009@live.se>
2022-02-06 12:08:32 +0100

    added user file

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -3,10 +3,12 @@ import jwtDecode from "jwt-decode";
 const tokenKey = "token";
 
 function getCurrentUser() {
+  //fake Login
   localStorage.setItem(
     tokenKey,
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWZjMTRlM2M1MmU5NGVhOTMwNjViNGYiLCJuYW1lIjoiQWxiaW4iLCJpYXQiOjE2NDM5MTA0MjZ9.z3mw4Sx3mfN6u99riY2zkQLDFds0NuYDmGuJLMYzdoM"
   );
+
   try {
     const token: any = localStorage.getItem(tokenKey);
     const user = jwtDecode(token);
