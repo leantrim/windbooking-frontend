4535bc4482f5c15d69e837928b0da578ca734c3e
albin <albinp2009@live.se>
2022-02-20 10:56:48 +0100

    TS Tech

--- src/components/TechTable.tsx
+++ src/components/TechTable.tsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getTech } from "../services/techService";
+import { Technician } from "../types/Technician";
 
 function TechTable() {
+  const [technician, setTechnician] = useState<[Technician]>([{}]);
+  useEffect(() => {
+    const fetchTech = async () => {
+      try {
+        const data: any = await getTech();
+        setTechnician(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchTech();
+  }, []);
+
   return <div>hej</div>;
 }
 
