bb4f59e7f1c56b521e3e3ec4b0df4e18b77cda62
Amanda <amanda.nilsson93@gmail.com>
2022-03-08 21:33:31 +0100

    fixed grid columns for windfarm.tsx

--- src/components/WindFarm.tsx
+++ src/components/WindFarm.tsx
@@ -35,36 +35,56 @@ function WindFarms() {
         </div>
         <div className="farms">
           <span className="name">Address</span>
-          <span className="name">Troubleshooting Manual</span>
+          <span className="name"></span>
           <span className="name">Wind Farm Details</span>
+          <span></span>
         </div>
         {windFarms?.map((windFarm) => (
           <div key={windFarm._id} className="row">
             <NavLink to={`/windfarm/${windFarm._id}`} className="name">
               {windFarm.address.name}
             </NavLink>
-            <span className="street">{windFarm.address.street}</span>
-            <span className="zipcode">{windFarm.address.zipcode}</span>
-            <span className="city">{windFarm.address.city}</span>
-            <span className="county">{windFarm.address.county}</span>
-            <span className="country">{windFarm.address.country}</span>
-            <span className="company">{windFarm.owner.company}</span>
-            <span className="manual">{windFarm.troubleshootingManual}</span>
-            <span className="hub-height">
-              {windFarm.windFarmDetails.hubHeight}
+            <div className="address">
+              <span className="street">{windFarm.address.street}</span>
+              <span className="zipcode">
+                {windFarm.address.zipcode} {windFarm.address.city}
+              </span>
+              <span className="county">{windFarm.address.county}</span>
+              <span className="country">{windFarm.address.country}</span>
+            </div>
+            <span className="company">
+              <h4>Company</h4>
+              {windFarm.owner.company}
             </span>
-            <span className="elevator-type">
-              {windFarm.windFarmDetails.elevatorType}
+            <span className="manual">
+              <h4>Troubleshooting Manual</h4>
+              {windFarm.troubleshootingManual}
             </span>
+            <div className="windturbine-details">
+              <span className="hub-height">
+                <h4>Hub Height</h4>
+                {windFarm.windFarmDetails.hubHeight}
+              </span>
+              <span className="number-of-wt">
+                <h4>Number of Wind Turbines</h4>
+                {windFarm.windFarmDetails.numberOfWindTurbines}
+              </span>
+            </div>
             <span className="commissioning-date">
+              <h4>Commissioning Date</h4>
               {windFarm.windFarmDetails.commissioningDate}
             </span>
-            <span className="safety-equipment">
-              {windFarm.windFarmDetails.safetyEquipment}
-            </span>
-            <span className="number-of-wt">
-              {windFarm.windFarmDetails.numberOfWindTurbines}
-            </span>
+            <div className="safety-details">
+              <span className="elevator-type">
+                <h4>Elevator Type</h4>
+                {windFarm.windFarmDetails.elevatorType}
+              </span>
+              <span className="safety-equipment">
+                <h4>Required Safety Equipment</h4>
+                {windFarm.windFarmDetails.safetyEquipment}
+              </span>
+            </div>
+
             <button
               onClick={() => handleDelete(windFarm)}
               className="fas fa-trash-alt"
